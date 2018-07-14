ae94a28fe34dc1e673af7a6e3e68ae8c46df1446
Kostas Pelelis <k.pelelis@thebeat.co>
2018-07-14 16:55:34 +0300

    Replace tabs with spaces

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,86 +4,86 @@ import { css } from "glamor";
 import { type Message } from "../FlowTypes";
 
 let messageCss = css({
-	marginTop: "0",
-	textAlign: self ? "right" : "left",
-	flex: "1 1 auto",
-	clear: "both"
+  marginTop: "0",
+  textAlign: self ? "right" : "left",
+  flex: "1 1 auto",
+  clear: "both"
 });
 
 type Props = {
-	messages: Array<Message>,
-	me: string
+  messages: Array<Message>,
+  me: string
 };
 
 const renderMessage = (message: Message, self: boolean) => {
-	return (
-		<div key={message.id} {...messageCss}>
-			<div
-				{...css({
-					position: "relative",
-					float: self ? "right" : "left"
-				})}
-			>
-				<div>{message.sender}</div>
-				<div
-					{...css({
-						marginTop: "0.5rem",
-						borderRadius: "8px",
-						padding: "1rem",
-						color: "white",
-						background: self ? "#5ddbc0" : "#f38258"
-					})}
-				>
-					{message.content}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div key={message.id} {...messageCss}>
+      <div
+        {...css({
+          position: "relative",
+          float: self ? "right" : "left"
+        })}
+      >
+        <div>{message.sender}</div>
+        <div
+          {...css({
+            marginTop: "0.5rem",
+            borderRadius: "8px",
+            padding: "1rem",
+            color: "white",
+            background: self ? "#5ddbc0" : "#f38258"
+          })}
+        >
+          {message.content}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const messagesContainerCss = css({
-	display: "flex",
-	flexFlow: "column",
-	width: "50rem",
-	height: "100%"
+  display: "flex",
+  flexFlow: "column",
+  width: "50rem",
+  height: "100%"
 });
 
 const messagesListCss = css({
-	listStyle: "none",
-	background: "white",
-	padding: "2rem",
-	color: "#000028",
-	overflowY: "scroll",
-	flex: "1 1 90%",
-	display: "flex",
-	flexDirection: "column"
+  listStyle: "none",
+  background: "white",
+  padding: "2rem",
+  color: "#000028",
+  overflowY: "scroll",
+  flex: "1 1 90%",
+  display: "flex",
+  flexDirection: "column"
 });
 
 export default class Chat extends React.Component {
-	chatContainerDOM = null;
+  chatContainerDOM = null;
 
-	scroll() {
-		if (this.chatContainerDOM) {
-			this.chatContainerDOM.scrollTop = this.chatContainerDOM.scrollHeight;
-		}
-	}
+  scroll() {
+    if (this.chatContainerDOM) {
+      this.chatContainerDOM.scrollTop = this.chatContainerDOM.scrollHeight;
+    }
+  }
 
-	componentDidMount() {
-		this.scroll();
-	}
+  componentDidMount() {
+    this.scroll();
+  }
 
-	componentDidUpdate() {
-		this.scroll();
-	}
+  componentDidUpdate() {
+    this.scroll();
+  }
 
-	render() {
-		const { messages, me } = this.props;
-		return (
-			<div {...messagesContainerCss}>
-				<div {...messagesListCss} ref={el => (this.chatContainerDOM = el)}>
-					{messages.map(m => renderMessage(m, m.sender === me.handle))}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { messages, me } = this.props;
+    return (
+      <div {...messagesContainerCss}>
+        <div {...messagesListCss} ref={el => (this.chatContainerDOM = el)}>
+          {messages.map(m => renderMessage(m, m.sender === me.handle))}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/UsersSidebar.jsx
+++ src/components/UsersSidebar.jsx
@@ -7,64 +7,64 @@ import { css } from "glamor";
 import { type User } from "../FlowTypes";
 
 type Props = {
-	users: Array<User>
+  users: Array<User>
 };
 
 let userContainerCss = css({
-	display: "flex",
-	marginBottom: "1rem"
+  display: "flex",
+  marginBottom: "1rem"
 });
 
 let avatarCss = css({
-	width: "3rem",
-	height: "3rem",
-	borderRadius: "50%",
-	border: "3px solid #8c8c9e"
+  width: "3rem",
+  height: "3rem",
+  borderRadius: "50%",
+  border: "3px solid #8c8c9e"
 });
 
 let userCss = css({
-	display: "flex",
-	flexDirection: "column",
-	justifyContent: "space-between",
-	marginLeft: "1rem",
-	padding: "0.1rem"
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  marginLeft: "1rem",
+  padding: "0.1rem"
 });
 
 let handleCss = {
-	fontWeight: "bold"
+  fontWeight: "bold"
 };
 
 const renderUser = (user: User) => {
-	return (
-		<li {...userContainerCss} key={user.handle}>
-			<img {...avatarCss} src={user.avatar} alt="avatar" />
-			<div {...userCss}>
-				<div>{user.handle}</div>
-				<div
-					style={css(
-						...{ color: user.status === "ONLINE" ? "#5ddbc0" : "#dddde5" }
-					)}
-				>
-					{user.status.toLowerCase()}
-				</div>
-			</div>
-		</li>
-	);
+  return (
+    <li {...userContainerCss} key={user.handle}>
+      <img {...avatarCss} src={user.avatar} alt="avatar" />
+      <div {...userCss}>
+        <div>{user.handle}</div>
+        <div
+          style={css(
+            ...{ color: user.status === "ONLINE" ? "#5ddbc0" : "#dddde5" }
+          )}
+        >
+          {user.status.toLowerCase()}
+        </div>
+      </div>
+    </li>
+  );
 };
 
 const userListCss = css({
-	listStyle: "none",
-	background: "#c0c0cb",
-	padding: "2rem",
-	color: "white",
-	width: "15rem",
-	overflowY: "scroll",
-	overflowX: "none"
+  listStyle: "none",
+  background: "#c0c0cb",
+  padding: "2rem",
+  color: "white",
+  width: "15rem",
+  overflowY: "scroll",
+  overflowX: "none"
 });
 
 export default class UsersSidebar extends React.Component<Props> {
-	render() {
-		const { users } = this.props;
-		return <ul {...userListCss}>{users.map(renderUser)}</ul>;
-	}
+  render() {
+    const { users } = this.props;
+    return <ul {...userListCss}>{users.map(renderUser)}</ul>;
+  }
 }
